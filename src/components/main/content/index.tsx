663543f7657853b412fe94c8a1941ebc46d19afc
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2022-08-12 22:43:27 +0200

    add: Añadido contenido
    Cargar contenido después de realizar login correctamente

---
+++ src/components/main/content/index.tsx
@@ -0,0 +1,20 @@
+import { View } from 'react-native'
+
+import style from './style'
+
+import Text from '../../text'
+
+
+const Content = () => {
+
+
+    return (
+
+        <View style={style.container}>
+            <Text type='h1'>Content</Text>
+        </View>
+
+    )
+}
+
+export default Content

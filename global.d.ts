92c651568f47b45ab13ffac9446c67bcbb53396a
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2022-08-12 23:39:59 +0200

    fix: permitir imagenes png en ámbito local

--- global.d.ts
+++ global.d.ts
@@ -2,5 +2,10 @@ declare const Proxy: {
     new <T extends object>(
         target: { results: { [key in string]?: T }; apiInstance: ApisauceInstance },
         handler: ProxyHandler<T, string>,
-    ): { [key: string]: Promise<T> };
-};
+    ): { [key: string]: Promise<T> }
+}
+
+declare module "*.png" {
+    const value: any;
+    export = value;
+}

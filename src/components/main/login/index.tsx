d8ccc45dc711106d76317896407036db095d19de
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2022-08-12 22:41:55 +0200

    add: Añadir contenido de login

---
+++ src/components/main/login/index.tsx
@@ -0,0 +1,20 @@
+import { View } from 'react-native'
+
+import style from './style'
+
+import Text from '../../text'
+
+
+const Login = () => {
+
+
+    return (
+
+        <View style={style.container}>
+            <Text type='h1'>Login</Text>
+        </View>
+
+    )
+}
+
+export default Login
---
+++ src/components/main/login/style.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native'
+
+const styles =
+{
+    container: {
+        padding: 20
+    }
+}
+
+export default StyleSheet.create(styles)

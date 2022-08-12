9a8b30aca1c4582e665e4f4b7146b634a9e29380
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2022-08-12 22:41:41 +0200

    add: Contenido de precarga de la aplicacción

---
+++ src/components/main/preaload/index.tsx
@@ -0,0 +1,20 @@
+import { View } from 'react-native'
+
+import style from './style'
+
+import Text from '../../text'
+
+
+const Preload = () => {
+
+
+    return (
+
+        <View style={style.container}>
+            <Text type='h1'>preaload</Text>
+        </View>
+
+    )
+}
+
+export default Preload

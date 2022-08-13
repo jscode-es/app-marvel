6fad116f6b17581e8b87192297a421bfb783f26f
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2022-08-14 00:22:32 +0200

    refactor: Ampliación de estado en el login

--- src/providers/src/login.tsx
+++ src/providers/src/login.tsx
@@ -1,13 +1,23 @@
 import { createContext, useState } from 'react'
 
+import { LoginContextInterface } from '../../interface/ILoginContext'
+
 const LoginContext = createContext(null)
 
 export function LoginProvider({ children }: any) {
 
     const [isLogin, setLogin] = useState(false)
+    const [isShow, setShow] = useState(false)
+
+    const value = {
+        isLogin,
+        setLogin,
+        isShow,
+        setShow
+    }
 
     return (
-        <LoginContext.Provider value={{ isLogin, setLogin }}>
+        <LoginContext.Provider value={value}>
             {children}
         </LoginContext.Provider>
     )

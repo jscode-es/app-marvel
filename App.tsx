cb5a34795cd61c41beb5a75c6af81ee2a381329f
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2022-08-14 00:29:08 +0200

    refactor: Forzar sistema Portrait y implementar providers

--- App.tsx
+++ App.tsx
@@ -1,2 +1,17 @@
 import Main from './src/components/main'
-export default () => <Main />
+import * as ScreenOrientation from 'expo-screen-orientation'
+
+import { LoginProvider } from './src/providers/src/login'
+import { ApiRequestProvider } from './src/providers/src/api'
+
+export default () => {
+
+    ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT)
+
+    return (
+        <LoginProvider>
+            <ApiRequestProvider>
+                <Main />
+            </ApiRequestProvider>
+        </LoginProvider>)
+}

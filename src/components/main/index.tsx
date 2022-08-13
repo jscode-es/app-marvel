0ccbfa88c8a4bcbfa0a6b35efbc828eca1dffa7f
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2022-08-14 00:25:07 +0200

    fix: Eliminación del sistema provider hell
    El algoritmo no retorna los estados, revisar

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -2,31 +2,17 @@ import { View } from 'react-native'
 
 import style from './style'
 
-import Providers from '../../providers'
-
-import { LoginProvider } from '../../providers/src/login'
-import { ApiRequestProvider } from '../../providers/src/api'
-import { PreloadProvider } from '../../providers/src/preload'
-
 import Preload from './preaload'
 import Login from './login'
 /*import Content from './content' */
 
 const Main = () => {
 
-    const providers = [
-        <PreloadProvider key='preloadProvider' />,
-        <LoginProvider key='loginProvider' />,
-        <ApiRequestProvider key='apiRequestProvider' />
-    ]
-
     return (
-        <Providers providers={providers}>
-            <View style={style.container}>
-                <Preload />
-                <Login />
-            </View>
-        </Providers>
+        <View style={style.container}>
+            <Preload />
+            <Login />
+        </View>
     )
 }
 

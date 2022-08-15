24decbf646207605c1d80c01b757e432937ca988
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2022-08-15 19:01:40 +0200

    refactor: ocultar status bar

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import Main from './src/components/main'
 import * as ScreenOrientation from 'expo-screen-orientation'
-
+import { StatusBar } from 'react-native'
 import { LoginProvider } from './src/providers/src/login'
 import { ApiRequestProvider } from './src/providers/src/api'
 
 export default () => {
 
     ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT)
+    StatusBar.setHidden(true)
 
     return (
         <LoginProvider>

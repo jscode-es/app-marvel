0053633a85c29a94028ae862f52a7379a899a8b9
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2022-08-15 18:55:45 +0200

    refactor: modificación de los estilos de la lista

--- src/components/heroesList/style.ts
+++ src/components/heroesList/style.ts
@@ -3,13 +3,69 @@ import { StyleSheet } from 'react-native'
 const styles: any =
 {
     container: {
-        backgroundColor: 'white',
+
+    },
+    list: {
+        width: '100%',
+        height: 550,
+        paddingHorizontal: 10
+    },
+    item: {
+        /*  aspectRatio: 1, */
+        width: '100%',
+        flex: 1,
+    },
+    itemContent: {
+        margin: 10,
+        height: 200,
+        backgroundColor: 'blue',
+        flex: 1,
         borderRadius: 10,
-        paddingVertical: 10,
-        paddingHorizontal: 20,
-        marginHorizontal: 30,
-        marginBottom: 10
+        overflow: 'hidden'
+    },
+    itemImage: {
+        height: '100%',
+        width: '100%'
+    },
+    itemTitle: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginHorizontal: 10,
+        marginBottom: 15
+    },
+    itemName: {
+        color: 'white',
+        fontSize: 15,
+        opacity: .5,
+        width: 110
+    },
+    titleList: {
+        color: 'white',
+        marginLeft: 25
+    },
+    subtitleList: {
+        marginLeft: 25,
+        color: '#2196f3',
+        paddingBottom: 10
+    },
+    itemBook: {
+        backgroundColor: '#2196f3',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderRadius: 5,
+        overflow: 'hidden',
+        paddingVertical: 2,
+        paddingHorizontal: 5
+    },
+    itemNum: {
+        marginLeft: 5,
+        color: 'white',
+        fontSize: 15,
     }
+
 }
 
 export default StyleSheet.create(styles)

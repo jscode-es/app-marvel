267390b1f8e5cd84f488cf463787c57d3670630e
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2022-08-15 18:54:30 +0200

    refactor: añadir dotenv en los test

--- jest.config.ts
+++ jest.config.ts
@@ -2,10 +2,19 @@ import type { Config } from '@jest/types'
 
 
 const config: Config.InitialOptions = {
+    preset: "react-native",
     verbose: true,
-    transform: {
+    setupFiles: ["dotenv/config"],
+    /* transform: {
         '^.+\\.tsx?$': 'ts-jest',
-    },
+    }, */
+    /*  moduleNameMapper: {
+         '^react-native$': 'react-native-web',
+         '^react-native-config$': 'react-native-config',
+     } */
+    transformIgnorePatterns: [
+        "node_modules/(?!(@react-native|react-native)/)"
+    ]
 }
 
 export default config

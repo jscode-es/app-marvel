5687715e3359a82b814303707e08bd48d32120d1
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2022-08-15 22:13:01 +0200

    refactor: añadido provider de héroe

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import * as ScreenOrientation from 'expo-screen-orientation'
 import { StatusBar } from 'react-native'
 import { LoginProvider } from './src/providers/src/login'
 import { ApiRequestProvider } from './src/providers/src/api'
+import { HeroeProvider } from './src/providers/src/heroe'
 
 export default () => {
 
@@ -12,7 +13,9 @@ export default () => {
     return (
         <LoginProvider>
             <ApiRequestProvider>
-                <Main />
+                <HeroeProvider>
+                    <Main />
+                </HeroeProvider>
             </ApiRequestProvider>
         </LoginProvider>)
 }

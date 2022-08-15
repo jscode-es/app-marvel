3383d5c3eae40c60258e9ebe774524cb0556b5bc
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2022-08-15 19:00:53 +0200

    refactor: controlador para los inputs

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,10 +1,20 @@
 import { View, TextInput } from 'react-native'
-
+import { Controller } from 'react-hook-form'
 import styles from './style'
 
-const Input = ({ ...restOfProps }) => {
+const Input = ({ control, name, required = false, ...restOfProps }) => {
 
-    return <View><TextInput style={styles.container} {...restOfProps} /></View>
+    return (
+        <View style={styles.container}>
+            <Controller
+                control={control}
+                name={name}
+                rules={{ required }}
+                render={({ field: { value, onChange, onBlur } }) => {
+                    return <TextInput {...restOfProps} value={value} onChangeText={onChange} onBlur={onBlur} />
+                }}
+            />
+        </View>)
 }
 
 export default Input

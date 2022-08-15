3a4c1eb9b82f50ed3546242e34c75a0a44aada06
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2022-08-15 19:02:09 +0200

    refactor: implementación de los test unitarios

--- test/unit/utils.test.ts
+++ test/unit/utils.test.ts
@@ -1,16 +1,29 @@
 import getAuthQueryStringParams from '../../src/utils/getAuthQueryStringParams'
 import getPaginationQueryStringParams from '../../src/utils/getPaginationQueryStringParams'
+import md5 from '../../src/utils/md5'
+import proxy from '../../src/utils/proxy'
+import getNavigatableUrl from '../../src/utils/getNavigatableUrl'
 
-describe('Utilidades', () => {
+describe('utils', () => {
 
-    test('getAuthQueryStringParams', () => {
+    test.skip('generate md5', () => {
 
-        const data = getAuthQueryStringParams()
+        const data = md5('datos a cifrar')
+        const hash = 'fb8281ae3f3cd77434ca29149a98fd5e'
 
-        expect(200).toEqual(200)
+        expect(data).toEqual(hash)
     })
 
-    test('getPaginationQueryStringParams', () => {
+    test.skip('getAuthQueryStringParams', () => {
+
+        const { PUBLIC_KEY, PRIVATE_KEY } = process.env
+        const data = getAuthQueryStringParams()
+        const hash = md5(`${data.ts}${PRIVATE_KEY}${PUBLIC_KEY}`)
+
+        expect(hash).toEqual(data.hash)
+    })
+
+    test.skip('getPaginationQueryStringParams', () => {
 
         const maxResults = 0
         const page = 0
@@ -18,6 +31,26 @@ describe('Utilidades', () => {
         const data = getPaginationQueryStringParams(maxResults, page)
 
         expect(200).toEqual(200)
+    })
+
+    test.skip('marvel proxy', async () => {
+
+        let a = await (proxy as any).apiInstance.get()
+
+        console.log(a)
+
+        expect(200).toEqual(200)
+
+    })
+
+    test('getNavigableUrl', async () => {
+
+        let a = getNavigatableUrl('https://gateway.marvel.com/v1/public/characters')
+
+        console.log(a)
+
+        expect(200).toEqual(200)
+
     })
 
 })

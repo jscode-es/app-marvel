73ab93fbed52c7726da30c0ab2e2d592ba7ebb78
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2022-09-07 09:59:00 +0200

    fix: cambio de tabulado

--- src/utils/proxy.ts
+++ src/utils/proxy.ts
@@ -3,46 +3,51 @@ import { MarvelResponse } from '../types/marvel/MarvelResponse'
 import { ApisauceInstance } from 'apisauce'
 
 const target = {
-    apiInstance: create({ baseURL: 'https://gateway.marvel.com' }),
-    results: {}
+	apiInstance: create({ baseURL: 'https://gateway.marvel.com' }),
+	results: {}
 }
 
 const get = <T extends MarvelResponse>(target: { results: { [key in string]?: T } }, url: string) => {
 
-    const values = target
+	const values = target
 
-    return new Promise<T>(async (res, rej) => {
+	return new Promise<T>(async (res, rej) => {
 
-        if (values.results.hasOwnProperty(url)) {
-            res(values.results[url] as T)
-            return
-        }
+		if (values.results.hasOwnProperty(url)) {
+			res(values.results[url] as T)
+			return
+		}
 
-        try {
+		try {
 
-            const response = await (target as {
-                results: { [key in string]?: T };
-                apiInstance: ApisauceInstance;
-            }).apiInstance.get<T>(url)
+			const response = await (target as {
+				results: { [key in string]?: T };
+				apiInstance: ApisauceInstance;
+			}).apiInstance.get<T>(url)
 
-            const { data } = response
+			const { data } = response
 
-            if (response.originalError?.response?.status !== 200 || !data) {
-                throw new Error('Error fetching data')
-            }
+			if (response.originalError?.response?.status !== 200 || !data) {
+				throw new Error('Error fetching data')
+			}
 
-            (target as { results: { [key in string]?: T } }).results[url] = data
+			(target as { results: { [key in string]?: T } }).results[url] = data
 
-            return data
+			return data
 
-        } catch (error) { rej(error) }
-    })
+		} catch (error) {
+
+			console.log('============================================')
+
+			rej(error)
+		}
+	})
 }
 
 const set = (target: any, url: string, value: any) => {
 
-    target.results[url] = value
-    return true
+	target.results[url] = value
+	return true
 }
 
 const handler = { ...get, ...set }

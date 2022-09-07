499ad04631f1cd03e8c3863cf9f2a9754ac8e18e
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2022-09-07 10:01:17 +0200

    fix: recuperación de variable de entorno

--- src/utils/getAuthQueryStringParams.ts
+++ src/utils/getAuthQueryStringParams.ts
@@ -3,17 +3,17 @@ import md5 from './md5'
 
 const getAuthQueryStringParams = (): AuthQuery => {
 
-    const { PUBLIC_KEY, PRIVATE_KEY } = process.env
+	const { PUBLIC_KEY, PRIVATE_KEY } = (globalThis as any).env
 
-    /* const ts = Date.now().toString() */
-    const ts = '1'
-    const hash = md5(`${ts}${PRIVATE_KEY}${PUBLIC_KEY}`)
+	/* const ts = Date.now().toString() */
+	const ts = '1'
+	const hash = md5(`${ts}${PRIVATE_KEY}${PUBLIC_KEY}`)
 
-    return {
-        apikey: PUBLIC_KEY,
-        ts,
-        hash
-    }
+	return {
+		apikey: PUBLIC_KEY,
+		ts,
+		hash
+	}
 }
 
 export default getAuthQueryStringParams

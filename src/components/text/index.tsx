885049b6ca4a3b1dde6e77c0681498953ea40530
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2022-09-07 10:04:47 +0200

    perf: Cambio de tabulado

--- src/components/text/index.tsx
+++ src/components/text/index.tsx
@@ -2,22 +2,22 @@ import { Text } from 'react-native'
 
 import styles from './style'
 
-const Content = ({ children, type = 'p', style = {}, ...restOfProps }) => {
+const Content = ({ children, type = 'p', style = {}, ...restOfProps }: any) => {
 
-    const styleText = [
-        styles.text,
-        type === 'h1' && styles.h1,
-        type === 'h2' && styles.h2,
-        type === 'h3' && styles.h3,
-        type === 'h4' && styles.h4,
-        type === 'h5' && styles.h5,
-        type === 'p' && styles.p,
-        type === 'span' && styles.span,
-        type === 'small' && styles.small,
-        style
-    ]
+	const styleText = [
+		styles.text,
+		type === 'h1' && styles.h1,
+		type === 'h2' && styles.h2,
+		type === 'h3' && styles.h3,
+		type === 'h4' && styles.h4,
+		type === 'h5' && styles.h5,
+		type === 'p' && styles.p,
+		type === 'span' && styles.span,
+		type === 'small' && styles.small,
+		style
+	]
 
-    return <Text style={styleText} {...restOfProps}>{children}</Text>
+	return <Text style={styleText} {...restOfProps}>{children}</Text>
 }
 
 export default Content

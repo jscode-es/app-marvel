a61b9435db1dc352d88dd159fef458959821e0de
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2022-09-07 10:00:57 +0200

    fix: Paginación dinámica

--- src/utils/getNavigatableUrl.ts
+++ src/utils/getNavigatableUrl.ts
@@ -1,14 +1,14 @@
 import getAuthQueryStringParams from "./getAuthQueryStringParams"
 import getPaginationQueryStringParams from "./getPaginationQueryStringParams"
 
-export default (url: string) => {
+export default (url: string, { offset, limit }) => {
 
-    const newUrl = new URL(url)
+	const newUrl = new URL(url)
 
-    Object.entries({
-        ...getAuthQueryStringParams(),
-        ...getPaginationQueryStringParams(20, 1),
-    }).forEach(([key, value]) => newUrl.searchParams.append(key, value))
+	Object.entries({
+		...getAuthQueryStringParams(),
+		...getPaginationQueryStringParams(limit, offset),
+	}).forEach(([key, value]) => newUrl.searchParams.append(key, value))
 
-    return newUrl.toString()
+	return newUrl.toString().replace('/?', '?')
 }

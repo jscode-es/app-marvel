3ece9f66bd6bc137120e6408ca5c8209151a83c9
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2022-09-07 10:00:36 +0200

    fix: datos dinámicos de búsqueda

--- src/utils/getPaginationQueryStringParams.ts
+++ src/utils/getPaginationQueryStringParams.ts
@@ -1,14 +1,14 @@
 import { PaginationQuery } from "../types/PaginationQuery"
 
-const getPaginationQueryStringParams = (maxResults: number, page: number): PaginationQuery => {
+const getPaginationQueryStringParams = (maxResults: number = 20, page: number = 0): PaginationQuery => {
 
-    //TODO: devolver los parametros de paginación para el 
-    //listado de héroes con ${maxResults} resultados por página y página ${page}
+	//TODO: devolver los parametros de paginación para el 
+	//listado de héroes con ${maxResults} resultados por página y página ${page}
 
-    return {
-        limit: '1',
-        offset: '0'
-    }
+	return {
+		limit: String(maxResults),
+		offset: String(maxResults * page)
+	}
 }
 
 export default getPaginationQueryStringParams

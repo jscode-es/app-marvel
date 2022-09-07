422b71d092c21e7457ff021a33d7a66fd7a3a038
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2022-09-07 09:56:27 +0200

    add: Texto en castellano

---
+++ src/assets/i18n/es.ts
@@ -0,0 +1,18 @@
+export default {
+	screen_1: {
+		title: 'Mira tus cómics favoritos de Marvel',
+		subtitle: 'Conectate para disfrutar del contenido',
+		btn_init: 'Inicia sesión',
+		btn_register: 'Registrate y disfruta',
+	},
+	screen_2: {
+		title: 'Todo el mundo necesita un héroe',
+		subtitle: 'Informate de todo que hacen tus superhéroes',
+		btn_init: 'Acceder a tus comics',
+		btn_restore: 'Recuperar',
+		btn_error: 'Datos incorrectos'
+	},
+	screen_3: {
+
+	}
+}

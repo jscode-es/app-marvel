1cc87f461769a66b7e4ac2264605082a680bbe08
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2022-09-07 09:54:33 +0200

    fix: Declaraciones de tipos

--- global.d.ts
+++ global.d.ts
@@ -1,11 +1,15 @@
 declare const Proxy: {
-    new <T extends object>(
-        target: { results: { [key in string]?: T }; apiInstance: ApisauceInstance },
-        handler: ProxyHandler<T, string>,
-    ): { [key: string]: Promise<T> }
+	new <T extends object>(
+		target: { results: { [key in string]?: T }; apiInstance: ApisauceInstance },
+		handler: ProxyHandler<T, string>,
+	): { [key: string]: Promise<T> }
 }
 
 declare module "*.png" {
-    const value: any;
-    export = value;
+	const value: any
+	export = value
+}
+
+declare module globalThis {
+	const env: any
 }
